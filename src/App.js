889c81eb3e00f58f77f2576dc34d6356271933d1
RedMaster974 <paulkbidi57@gmail.com>
2023-11-02 15:08:06 +0400

    creation des fonction et affichage des cartes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Card from './Card'
+import {
+  AiOutlineArrowUp,
+  AiOutlineArrowDown,
+} from "react-icons/ai";
 
 function App() {
+    const [searchValue, setSearchValue] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [sort, setSort] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const request = await fetch(
+          "https://api.themoviedb.org/3/search/movie?api_key=da3f98d1b5a2a202896dd26efd46986c&language=fr-FR&query=" + searchValue
+        );
+        const data = await request.json();
+        setMovies(data.results)
+        console.log(data.results);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>React Movies</h1>
+        <div className="routingBtn">
+          <button>Accueuil</button>
+          <button>Coups de coeur</button>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Titre du film"
+            onInput={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
+          <input type="submit" value="Rechercher" />
+        </form>
+        <div className="sortContainer">
+          <button
+            onClick={() => {
+              setSort("top");
+            }}
+          >
+            {" "}
+            <AiOutlineArrowUp /> TOP
+          </button>
+          <button
+            onClick={() => {
+              setSort("flop");
+            }}
+          >
+            {" "}
+            <AiOutlineArrowDown /> FLOP
+          </button>
+        </div>
       </header>
+      <main>
+        {movies
+          .sort((a, b) => {
+            if (sort == "top") {
+              return b.vote_average - a.vote_average;
+            } else if (sort == "flop") {
+              return a.vote_average - b.vote_average;
+            } else {
+              return 0;
+            }
+          })
+          .map((movie, index) => {
+            return <Card movie={movie} key={index} />;
+          })}
+      </main>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Card.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+} from "react-icons/ai";
+
+function Card({ movie }) {
+    var image = "";
+    if (movie.backdrop_path == null){
+        image = movie.poster_path;
+    } else {
+        image = movie.backdrop_path;
+    }
+  return (
+    <div className="card">
+      <img
+        src={"https://image.tmdb.org/t/p/w300/" + image}
+        alt={"Affiche du film" + movie.title}
+      />
+      <p>{movie.title}</p>
+      <p>{movie.release_date}</p>
+      <p>{movie.vote_average}</p>
+      <p>{movie.overview}</p>
+      <AiOutlineHeart size={"30px"} className="heartIconCard" />
+    </div>
+  );
+}
+
+export default Card
